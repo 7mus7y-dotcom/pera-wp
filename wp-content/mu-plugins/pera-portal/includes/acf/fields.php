dbb74cee560885e89b60a16be005f61900bd7593
7mus7y-dotcom <7mus7y@gmail.com>
2026-02-25 18:48:05 +0300

    Fix Pera Portal scaffold constants and bootstrap ordering

--- wp-content/mu-plugins/pera-portal/includes/acf/fields.php
+++ wp-content/mu-plugins/pera-portal/includes/acf/fields.php
@@ -8,11 +8,22 @@ function pera_portal_acf_save_json_path($path)
 {
     return PERA_PORTAL_PATH . '/acf-json';
 }
-add_filter('acf/settings/save_json', 'pera_portal_acf_save_json_path');
 
 function pera_portal_acf_load_json_paths($paths)
 {
-    $paths[] = PERA_PORTAL_PATH . '/acf-json';
+    if (!is_array($paths)) {
+        $paths = [];
+    }
+
+    $json_path = PERA_PORTAL_PATH . '/acf-json';
+    if (!in_array($json_path, $paths, true)) {
+        $paths[] = $json_path;
+    }
+
     return $paths;
 }
-add_filter('acf/settings/load_json', 'pera_portal_acf_load_json_paths');
+
+if (function_exists('acf') || class_exists('ACF')) {
+    add_filter('acf/settings/save_json', 'pera_portal_acf_save_json_path');
+    add_filter('acf/settings/load_json', 'pera_portal_acf_load_json_paths');
+}
