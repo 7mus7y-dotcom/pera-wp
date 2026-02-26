b02047f057ac7e950c56011098c90c909f3fd79b
7mus7y-dotcom <7mus7y@gmail.com>
2026-02-26 14:50:58 +0300

    Portal: align frontend wrapper + hero markup with CRM pattern

--- wp-content/mu-plugins/pera-portal/includes/shortcodes/portal-shortcode.php
+++ wp-content/mu-plugins/pera-portal/includes/shortcodes/portal-shortcode.php
@@ -18,10 +18,12 @@ function pera_portal_render_shortcode($atts = [])
     $atts = shortcode_atts([
         'building' => '',
         'floor' => '',
+        'wrap' => '1',
     ], $atts, PERA_PORTAL_SHORTCODE_TAG);
 
     $building_id = absint($atts['building']);
     $floor_id = absint($atts['floor']);
+    $wrap_main = isset($atts['wrap']) ? (bool) absint((string) $atts['wrap']) : true;
 
     if (function_exists('pera_portal_mark_assets_needed')) {
         pera_portal_mark_assets_needed();
@@ -41,9 +43,18 @@ function pera_portal_render_shortcode($atts = [])
     }
 
     ob_start();
+    $previous_wrap_state = $GLOBALS['pera_portal_wrap_main'] ?? null;
+    $GLOBALS['pera_portal_wrap_main'] = $wrap_main;
+
     $template_path = PERA_PORTAL_PATH . '/templates/portal-shell.php';
     if (file_exists($template_path)) {
         include $template_path;
+    }
+
+    if (null === $previous_wrap_state) {
+        unset($GLOBALS['pera_portal_wrap_main']);
+    } else {
+        $GLOBALS['pera_portal_wrap_main'] = $previous_wrap_state;
     }
 
     return (string) ob_get_clean();
