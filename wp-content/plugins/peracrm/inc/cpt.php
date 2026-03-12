34a6726b3c6499c0e20155f21941125bdf2b7f96
7mus7y-dotcom <7mus7y@gmail.com>
2026-03-12 11:44:32 +0000

    Add PeraCRM CPT and CLI components

---
+++ wp-content/plugins/peracrm/inc/cpt.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+function peracrm_register_cpt_crm_client()
+{
+    $labels = [
+        'name' => 'CRM Clients',
+        'singular_name' => 'CRM Lead',
+        'add_new' => 'Add lead',
+        'add_new_item' => 'Add lead',
+        'edit_item' => 'Lead information',
+    ];
+
+    register_post_type('crm_client', [
+        'labels' => $labels,
+        'public' => false,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_icon' => 'dashicons-id',
+        'supports' => ['title'],
+        'show_in_rest' => true,
+        'capability_type' => ['crm_client', 'crm_clients'],
+        'map_meta_cap' => true,
+        'has_archive' => false,
+        'rewrite' => false,
+    ]);
+}
