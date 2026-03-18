7e0890cb3d977c51691897edfc1f99ea7551dc3f
7mus7y-dotcom <7mus7y@gmail.com>
2026-03-18 19:11:14 +0300

    Add CRM header logo block parity

--- wp-content/plugins/peracrm/inc/views/shell/header.php
+++ wp-content/plugins/peracrm/inc/views/shell/header.php
@@ -8,15 +8,15 @@ do_action('get_header', null, array());
 
 $site_name = (string) get_bloginfo('name');
 $logo_id = (int) get_theme_mod('custom_logo');
-$theme_logo_html = '';
+$custom_logo_html = '';
 
 if ($logo_id > 0) {
-    $theme_logo_html = wp_get_attachment_image($logo_id, 'full', false, array('class' => 'custom-logo pera-site-logo-image', 'alt' => $site_name, 'loading' => 'eager'));
+    $custom_logo_html = wp_get_attachment_image($logo_id, 'full', false, array('class' => 'custom-logo pera-site-logo-image peracrm-shell-logo-image', 'alt' => $site_name, 'loading' => 'eager'));
 }
 
-$plugin_fallback_path = trailingslashit(PERACRM_PATH) . 'logos-icons/pera-logo.svg';
-$plugin_fallback_url = trailingslashit(PERACRM_URL) . 'logos-icons/pera-logo.svg';
-$has_plugin_fallback = file_exists($plugin_fallback_path);
+$plugin_logo_path = trailingslashit(PERACRM_PATH) . 'logos-icons/pera-logo.svg';
+$plugin_logo_url = trailingslashit(PERACRM_URL) . 'logos-icons/pera-logo.svg';
+$has_plugin_logo = file_exists($plugin_logo_path);
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
@@ -32,17 +32,18 @@ $has_plugin_fallback = file_exists($plugin_fallback_path);
 
 <header id="site-header" class="site-header peracrm-shell-header" role="banner">
   <div class="container header-inner">
-    
-    <div class="site-branding">
-      <a class="site-logo logo-pera" href="<?php echo esc_url(home_url('/')); ?>" rel="home" aria-label="<?php echo esc_attr($site_name); ?>">
-        <?php if ('' !== $theme_logo_html) : ?>
-          <?php echo $theme_logo_html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-        <?php elseif ($has_plugin_fallback) : ?>
-          <img src="<?php echo esc_url($plugin_fallback_url); ?>" class="pera-site-logo-image peracrm-shell-logo-image" alt="<?php echo esc_attr($site_name); ?>" loading="eager">
-        <?php else : ?>
-          <span class="peracrm-shell-logo-text"><?php echo esc_html($site_name); ?></span>
-        <?php endif; ?>
-      </a>
+    <div class="site-branding peracrm-shell-branding">
+      <div class="peracrm-shell-logo-block">
+        <a class="site-logo logo-pera peracrm-shell-logo-link" href="<?php echo esc_url(home_url('/')); ?>" rel="home" aria-label="<?php echo esc_attr($site_name); ?>">
+          <?php if ('' !== $custom_logo_html) : ?>
+            <?php echo $custom_logo_html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+          <?php elseif ($has_plugin_logo) : ?>
+            <img src="<?php echo esc_url($plugin_logo_url); ?>" class="pera-site-logo-image peracrm-shell-logo-image" alt="<?php echo esc_attr($site_name); ?>" loading="eager">
+          <?php else : ?>
+            <span class="peracrm-shell-logo-text"><?php echo esc_html($site_name); ?></span>
+          <?php endif; ?>
+        </a>
+      </div>
     </div>
 
     <div class="peracrm-header-actions">
