8e1ca84e34a13b2756dffe5a13a5434bf507e0f8
7mus7y-dotcom <7mus7y@gmail.com>
2026-04-28 12:43:00 +0300

    Refactor Meta Pixel into dedicated analytics module

--- wp-content/themes/hello-elementor-child/inc/modules/analytics.php
+++ wp-content/themes/hello-elementor-child/inc/modules/analytics.php
@@ -4,6 +4,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once get_stylesheet_directory() . '/inc/modules/analytics/ahrefs.php';
+require_once get_stylesheet_directory() . '/inc/modules/analytics/meta.php';
+
+if ( ! function_exists( 'pera_analytics_head_scripts' ) ) {
+	function pera_analytics_head_scripts(): void {
+		if ( is_admin() || is_user_logged_in() ) {
+			return;
+		}
+
+		pera_analytics_render_ahrefs();
+		pera_analytics_meta_pixel();
+	}
+}
+
+if ( ! is_admin() ) {
+	add_action( 'wp_head', 'pera_analytics_head_scripts', 20 );
+}
+
 require_once get_stylesheet_directory() . '/inc/modules/analytics/install.php';
 require_once get_stylesheet_directory() . '/inc/modules/analytics/bots.php';
 require_once get_stylesheet_directory() . '/inc/modules/analytics/queries.php';
